cde8e7d8a309a99dc9857416e539e57ebc57d3ac
Ale Davidovich <daviroeik@gmail.com>
2016-09-22 15:14:03 -0400

    clase 5 - ej 1

---
+++ C5/E1/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>EJ #2</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="estilo.css">
+    </head>
+    <body>
+        <div id="wrap">
+            <form>
+                <table>
+                    <tr>
+                        <td>Favor ingrese una palabra :</td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name='palabra'></td>
+                    </tr>
+                    <tr>
+                        <td><button type="submit">Analizar!</button></td>
+                    </tr>
+                </table>
+            </form>
+            
+            <?php
+                
+                $palabra = $_GET['palabra'];
+                
+                if(isset($palabra)
+                {
+                    $reverso = strrev($palabra);
+                    if($palabra == $reverso)
+                    {
+                        echo 'La palabra ingresada es un palindromo :)';
+                    }
+                    /*
+                    else
+                    {
+                        echo 'La palabra ingresada no es un palindromo :('       
+                    }*/
+                }
+            ?>
+        <div>
+        
