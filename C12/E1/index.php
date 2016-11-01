02156636aed4856e5230d82879d70b760fbcdcd5
Ale Davidovich <daviroeik@gmail.com>
2016-11-01 10:12:54 -0300

    C12 tabla

--- C12/E1/index.php
+++ C12/E1/index.php
@@ -7,45 +7,69 @@
         
     </head>
     <body>
+        <div id="wrap">
+            
+            <p>Ejercicio 12</p>
+            
+            <table>
+                <tr>
+                    <td>Nombre</td>
+                    <td>Apellido</td>
+                    <td>Cedula</td>
+                    <td>Direccion</td>
+                    <td>Edad</td>
+                    <td>Codigo</td>
+                </tr>
+            
         
-        Ejercicio 12
+            <?php
+            
+                include "persona.php";
+                include "empleado.php";
+                /*
+                $host="localhost";
+                $port="5432";
+                $dbname="clase12";
+                $user="postgres";
+                $password="postgres";
         
-        <?php
+                $cadenaConexion = "host=$host port=$port dbname=$dbname user=$user password=$password";
         
-            include "persona.php";
-            include "empleado.php";
-            /*
-            $host="localhost";
-            $port="5432";
-            $dbname="clase12";
-            $user="postgres";
-            $password="postgres";
-    
-            $cadenaConexion = "host=$host port=$port dbname=$dbname user=$user password=$password";
-    
-            $conexion = pg_connect($cadenaConexion) or die("<br/>Error en la Conexión: ".pg_last_error());
-            */
-            $nombre_persona = "Jorge";
-            $apellido_persona = "Ale";
-            $cedula_persona = "123321";
-            $direccion_persona = "eusebio ayala";
-            $edad_persona = "24";
-            $codigoEmpelado = "222001";
-            
-            $empleadito = new Empleado($nombre_persona, $apellido_persona, $cedula_persona, $direccion_persona, $edad_persona, $codigoEmpelado);
-            echo"<br/>";
-            echo "Nombre: ".$empleadito->getNombrePersona()."<br/>";
-            echo "Apellido: ".$empleadito->getApellidoPersona()."<br/>";
-            echo "Cedula: ".$empleadito->getCedulaPersona()."<br/>";
-            echo "Direccion: ".$empleadito->getDireccionPersona()."<br/>";
-            echo "Codigo: ".$empleadito->getCodigoEmpleado()."<br/>";
-            
-            
-            
-  
-    
-            
-           
-        ?>
+                $conexion = pg_connect($cadenaConexion) or die("<br/>Error en la Conexión: ".pg_last_error());
+                */
+                $nombre_persona = "Jorge";
+                $apellido_persona = "Ale";
+                $cedula_persona = "123321";
+                $direccion_persona = "eusebio ayala";
+                $edad_persona = "24";
+                $codigoEmpelado = "222001";
+                
+                $empleadito = new Empleado($nombre_persona, $apellido_persona, $cedula_persona, $direccion_persona, $edad_persona, $codigoEmpelado);
+                /*
+                echo"<br/>";
+                echo "Nombre: ".$empleadito->getNombrePersona()."<br/>";
+                echo "Apellido: ".$empleadito->getApellidoPersona()."<br/>";
+                echo "Cedula: ".$empleadito->getCedulaPersona()."<br/>";
+                echo "Direccion: ".$empleadito->getDireccionPersona()."<br/>";
+                echo "Codigo: ".$empleadito->getCodigoEmpleado()."<br/>";
+                */
+                echo"<br/>";
+                echo"
+                    <tr>
+                        <td>".$empleadito->getNombrePersona()."</td>
+                        <td>".$empleadito->getApellidoPersona()."</td>
+                        <td>".$empleadito->getCedulaPersona()."</td>
+                        <td>".$empleadito->getDireccionPersona()."</td>
+                        <td>".$empleadito->getEdadPersona()."</td>
+                        <td>".$empleadito->getCodigoEmpleado()."</td>
+                    </tr>
+                    ";
+                
+                
+                
+
+            ?>
+            </table>
+        </div>
     </body>
 </html>
